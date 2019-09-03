beecb933cf0a4543ef1755501dabdd0394f21eff
ocha <ochaochaocha3@gmail.com>
2019-09-03 15:03:12 +0900

    モジュール名と定数名を対象に加える

--- referral.sh
+++ referral.sh
@@ -4,7 +4,7 @@ OUTPUT_FILENAME='bcdice_naming.csv'
 
 bundle exec referral \
   --column=file,line,column,type,scope,name \
-  --type=module,class,constand_def,class_method,instance_method,local_var_assign,instance_var_assign,class_var_assign,global_var_assign \
+  --type=module,class,constant_def,class_method,instance_method,local_var_assign,instance_var_assign,class_var_assign,global_var_assign \
   --delimiter="," \
   --print-headers \
   vendor/BCDice/src/{,{dice,diceBot,utils}/}*.rb \
